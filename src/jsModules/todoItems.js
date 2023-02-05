6c4501e92bb68da289b8adefc33a2217217d34aa
Roquez <roquez94@gmail.com>
2023-02-05 11:17:01 -0500

    Created todo js, finish objects for todoItems

---
+++ src/jsModules/todoItems.js
@@ -0,0 +1,13 @@
+
+//todoItems holds the other functions properties & methods
+//attempting to follow SOLID oop principles
+
+//creates todo object-items with title, description, dueDate
+//..notes and checklist
+let todoItems = function (title,description,dueDate,priortity) {
+    
+
+};
+
+
+
